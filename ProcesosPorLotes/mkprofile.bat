13278875008f34f1fb24e16d95a3288da5d9077a
EnriqueNietoBlazquez <150673650+EnriqueNietoBlazquez@users.noreply.github.com>
2023-11-13 12:20:57 +0100

    Add files via upload
    BAT para crear el árbol de directorios

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM Función: Crear arbol de directorios
+
+REM Fecha: Lunes 13-11-2023
+
+REM Autor: Enrique Nieto Blázquez
+
+REM De esta forma, aunque sean más lineas, son mucho más simples y ergonomicas de leer para el usuario
+
+mkdir %USERPROFILE%\asir\docs
+mkdir %USERPROFILE%\dam\docs
+mkdir %USERPROFILE%\smr\docs
+mkdir %USERPROFILE%\daw\docs
+mkdir %USERPROFILE%\ceti\docs
+
+mkdir %USERPROFILE%\asir\pract
+mkdir %USERPROFILE%\dam\pract
+mkdir %USERPROFILE%\smr\pract
+mkdir %USERPROFILE%\daw\pract
+mkdir %USERPROFILE%\ceti\pract
+
+mkdir %USERPROFILE%\asir\exam
+mkdir %USERPROFILE%\dam\exam
+mkdir %USERPROFILE%\smr\exam
+mkdir %USERPROFILE%\daw\exam
+mkdir %USERPROFILE%\ceti\exam

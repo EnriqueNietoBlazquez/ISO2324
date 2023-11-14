fa69b3a27221a18ed484959e72843556aaba1ae7
EnriqueNietoBlazquez <150673650+EnriqueNietoBlazquez@users.noreply.github.com>
2023-11-14 14:04:01 +0100

    BAT para hacer la media de 3 parámetros

---
+++ ProcesosPorLotes/Media.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Autor: Enrique Nieto Blázquez
+rem Fecha: Martes 14-11-2023
+rem Funcion: Hacer la media de 3 números distintos.
+
+set /a media=(%1 + %2 + %3) / 3
+
+echo La media de %1, %2 y %3 es %media%
